f361d03d47e4f1257719d98bff0b6792b7664870
unclebobert <jacksonfu777@gmail.com>
2026-05-19 23:41:20 +1000

    add authentication flow on server and client

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,10 +5,50 @@ import { DiscordSDK } from '@discord/embedded-app-sdk'
 import './index.css'
 import App from './App.tsx'
 
+// Will eventually store the authenticated user's access_token
+let auth: null | Awaited<ReturnType<typeof discordSDK.commands.authenticate>> = null;
+
 const discordSDK = new DiscordSDK(import.meta.env.VITE_CLIENT_ID)
 
-await discordSDK.ready()
-console.log('Discord SDK is ready!')
+setupDiscordSDK().then(() => {
+  console.log("Discord SDK is authenticated");
+});
+
+async function setupDiscordSDK() {
+  await discordSDK.ready()
+  console.log('Discord SDK is ready!')
+
+  const { code } = await discordSDK.commands.authorize({
+    client_id: import.meta.env.VITE_DISCORD_CLIENT_ID,
+    response_type: "code",
+    state: "",
+    prompt: "none",
+    scope: [
+      "identify",
+      "guilds",
+      "applications.commands"
+    ],
+  });
+
+  // Retrieve an access_token from your activity's server
+  const response = await fetch("/api/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      code,
+    }),
+  });
+  const { access_token } = await response.json();
+
+  auth = await discordSDK.commands.authenticate({ access_token });
+  if (!auth) {
+    throw new Error("Failed to authenticate with Discord SDK");
+  }
+
+  console.log("Authenticated with Discord SDK", auth);
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,37 @@
 import { Hono } from 'hono'
 
-const app = new Hono()
+type Bindings = {
+  CLIENT_ID: string;
+  CLIENT_SECRET: string;
+};
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const app = new Hono<{ Bindings: Bindings }>();
+
+// NOTE: endpoints should never include /api since discord routes all requests
+// to /api/* to this server and removes the prefix
+// i.e. the client should always prepend /api before making requests to the server,
+// but the server should not include /api in its routes
+app.post('/token', async (c) => {
+  const code = await c.req.json().then(({code}) => code);
+  if (typeof code !== 'string') {
+    return c.json({ error: 'Invalid code' }, 400);
+  }
+  // Exchange the code for an access_token
+  const response = await fetch('https://discord.com/api/oauth2/token', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: new URLSearchParams({
+      client_id: c.env.CLIENT_ID,
+      client_secret: c.env.CLIENT_SECRET,
+      grant_type: "authorization_code",
+      code,
+    })
+  });
+
+  const data = await response.json();
+  return c.json(data);
+});
 
 export default app
