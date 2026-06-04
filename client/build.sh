48cd853a5b3ee26f8c61148c6fbabefedb8c7451
unclebobert <jacksonfu777@gmail.com>
2026-06-04 21:37:51 +1000

    attempt fix for build script

--- client/build.sh
+++ client/build.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+branch="$(git branch --show-current)"
+
 # export the environment variable for production/experimental
 # the client ID (not client secret) is public and safe to expose
-if [ "$CF_PAGES_BRANCH" = "master" ]; then
+if [ "$branch" = "master" ]; then
     export VITE_DISCORD_CLIENT_ID=1506187422281498684
-elif [ "$CF_PAGES_BRANCH" = "experimental" ]; then
+elif [ "$branch" = "experimental" ]; then
     export VITE_DISCORD_CLIENT_ID=1507619764284031016
 else
     # do not build for other branches
-    echo "Not building for branch $CF_PAGES_BRANCH"
-    exit 0
+    echo "Not building for branch $branch" >&2
+    exit 1
 fi
 pnpm run build
