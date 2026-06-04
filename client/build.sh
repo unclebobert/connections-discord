3d30f8b681592dc52d0626a895d12ee3ad279b04
unclebobert <jacksonfu777@gmail.com>
2026-06-04 21:21:38 +1000

    fix build script

--- client/build.sh
+++ client/build.sh
@@ -2,9 +2,9 @@
 
 # export the environment variable for production/experimental
 # the client ID (not client secret) is public and safe to expose
-if [ "$CF_PAGES_BRANCH" == "master" ]; then
+if [ "$CF_PAGES_BRANCH" = "master" ]; then
     export VITE_DISCORD_CLIENT_ID=1506187422281498684
-elif [ "$CF_PAGES_BRANCH" == "experimental" ]; then
+elif [ "$CF_PAGES_BRANCH" = "experimental" ]; then
     export VITE_DISCORD_CLIENT_ID=1507619764284031016
 else
     # do not build for other branches
