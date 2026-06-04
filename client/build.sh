222cd86063da66e24b5bb29072382181d7018e2b
unclebobert <jacksonfu777@gmail.com>
2026-06-04 21:27:12 +1000

    attempt debug issue

--- client/build.sh
+++ client/build.sh
@@ -1,4 +1,4 @@
-# !/bin/bash
+#!/bin/bash
 
 # export the environment variable for production/experimental
 # the client ID (not client secret) is public and safe to expose
@@ -8,6 +8,7 @@ elif [ "$CF_PAGES_BRANCH" = "experimental" ]; then
     export VITE_DISCORD_CLIENT_ID=1507619764284031016
 else
     # do not build for other branches
+    echo "Not building for branch $CF_PAGES_BRANCH"
     exit 0
 fi
 pnpm run build
